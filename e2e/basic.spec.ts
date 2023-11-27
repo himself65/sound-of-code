43e0f4a18f2617e46d9f057266b70d46456dd413
Alex Yang <himself65@outlook.com>
2023-11-27 14:15:37 -0600

    test: init e2e test

---
+++ e2e/basic.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('has header', async ({ page }) => {
+  await page.goto('/')
+  const header = page.locator('header')
+  await expect(header).toBeVisible()
+});
